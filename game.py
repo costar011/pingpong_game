b0feaaab45866614264202cc18fc21e55386ea8a
costar011 <dnaer456@gmail.com>
2024-11-04 07:31:06 +0900

    player write

--- game.py
+++ game.py
@@ -4,12 +4,11 @@ import time
 
 def right():
     if player.xcor() < 200:
-    player.forward(x+10, y)
+        player.forward(10)
 
 def left():
     if player.xcor() > -200:
-    player.backward(x+10, y)
-
+        player.backward(10)
 
 t.bgcolor('skyblue')
 t.setup(500, 700)
@@ -17,17 +16,14 @@ t.setup(500, 700)
 # 플레이어 설정
 player = t.Turtle()
 player.shape('square')
-player.shapesize(1,5)
+player.shapesize(1, 5)
 player.up()
 player.speed(0)
 player.goto(0, -270)
-
 
 t.listen()
 t.onkeypress(right, 'Right')
 t.onkeypress(left, 'Left')
 
-
-# ball
-
-ball - t.Turtle()
+# 창을 닫히지 않게 유지
+t.mainloop()
