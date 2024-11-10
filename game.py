058fa8f4980b1608c75c9646a94b4ac36ea9e99c
costar011 <dnaer456@gmail.com>
2024-11-11 07:15:43 +0900

    life

--- game.py
+++ game.py
@@ -26,6 +26,13 @@ t.onkeypress(right, 'Right')
 t.onkeypress(left, 'Left')
 
 game_on = True
+life = 3
+
+# 점수 설정
+t.up()
+t.ht()
+t.goto(0, 300)
+t.write(f"Life: {life}", False, 'center', ('', 20))
 
 ball_xspeed = 5
 ball_yspeed = 5
@@ -39,4 +46,14 @@ While game_on:
         ball_xspeed *= -1
     
     if ball.ycor() > 340:
+        ball_yspeed *= -1
+
+    if ball.ycor() < -340:
+        time.sleep(0.5)
+        ball.goto(0, 100)
+
+        ball_xspeed *= -1
+        ball_yspeed *= -1
+
+    if player.distance(ball) < 50 and -260 < ball.ycor() < -245:
         ball_yspeed *= -1
