cc02258c275b1c34630cb3edf26bef4e8a3b9f81
costar011 <dnaer456@gmail.com>
2024-11-10 23:38:36 +0900

    ball

--- game.py
+++ game.py
@@ -25,5 +25,18 @@ t.listen()
 t.onkeypress(right, 'Right')
 t.onkeypress(left, 'Left')
 
-# 창을 닫히지 않게 유지
-t.mainloop()
+game_on = True
+
+ball_xspeed = 5
+ball_yspeed = 5
+
+While game_on:
+    new_x = ball.xcor() + ball_xspeed
+    new_y = ball.ycor() + ball_yspeed
+    ball.goto(new_x, new_y)
+
+    if ball.xcor() > 240 or ball.xcor() < -240:
+        ball_xspeed *= -1
+    
+    if ball.ycor() > 340:
+        ball_yspeed *= -1
